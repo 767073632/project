ca80658866a9fb09cc17f7c75372c8918877b2b1
767073632 <767073632@qq.com>
2022-07-31 18:16:59 +0800

    3全局配置

---
+++ App.vue
@@ -0,0 +1,25 @@
+<script>
+	export default {
+		onLaunch: function() {
+			// 加载公共图标库
+			const domModule = weex.requireModule('dom')
+			domModule.addRule('fontFace', {
+			    'fontFamily': "iconfont",
+			    'src': "url('https://at.alicdn.com/t/font_1365296_2ijcbdrmsg.ttf')"
+			});
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "./common/free.css";
+	@import "./common/common.css";
+	@import "./common/free-icon.css";
+</style>
